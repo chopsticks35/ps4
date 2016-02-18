84cd243ecc34edd7817979995c54b4e68dc122d8
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-18 16:04:35 -0700

    first part one

---
+++ main.js
@@ -0,0 +1,20 @@
+//var isNumber = function(str) {
+//	return Number.isInteger(parseInt(str));
+//}
+//
+//var addNumbers = function(str) {
+//	var chars = str.split("");
+//	var total = 0;
+//	var i = 0
+//	while (i < chars.length) {
+//		if (isNumber(chars[i]) && isNumber(chars[i+1])) {
+//			total += parseInt(chars[i] + chars[i+1]);
+//			i += 2;
+//		}
+//		else if (isNumber(chars[i])) {
+//			total += parseInt(chars[i]);
+//		}
+//		i++;
+//	}
+//	return total;
+//}
